4114a5d4fec324f692522ec33a781fe3d0c01b52
iagribanov <115376983+iagribanov@users.noreply.github.com>
2022-10-26 19:21:29 +0300

    Update rna_transcription.py

--- rna_transcription.py
+++ rna_transcription.py
@@ -1,4 +1,6 @@
 def to_rna(dna_strand):
-    dna_strand = dna_strand.replace('A','U').replace('G','K').replace('C','O').replace('T','A')
-    dna_strand=dna_strand.replace('K','C').replace('O','G')
-    return dna_strand
+     """Вычисляет результат транскрипции РНК
+     :param dna_strand: строка ДНК из символов ACGT
+     :return: строка РНК из символов UGCA
+     """
+     return dna_strand # Удалите эту строку и напишите вместо неё ваш код
